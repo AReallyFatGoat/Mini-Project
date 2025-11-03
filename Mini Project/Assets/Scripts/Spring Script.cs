8068d61c0d4ac404045cf1b629a5f7f6abffd042
AReallyFatGoat <zachasaks@gmail.com>
2025-11-03 12:02:35 +0100

    Kinda working spring

--- Mini Project/Assets/Scripts/Spring Script.cs
+++ Mini Project/Assets/Scripts/Spring Script.cs
@@ -5,14 +5,31 @@ public class SpringScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private int downforce;
+    [SerializeField] private int upforce;
+    [SerializeField] private int springlimit;
 
     public void OnDownPress(InputAction.CallbackContext context)
     {
         bool pressed = context.performed;
 
-        while (pressed)
+        if (pressed)
         {
             rb.linearVelocity = new Vector3(0, downforce, 0);
         }
+        else
+        {
+            rb.linearVelocity = new Vector3(0, upforce, 0);
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y > springlimit)
+        {
+            rb.linearVelocity = new Vector3(0, 0, 0);
+            transform.position = new Vector3(transform.position.x, springlimit, transform.position.z);
+        }
     }
 }
+
+
